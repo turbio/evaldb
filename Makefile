d07d729a859ed614aed15984f2adf4b2089b5d8e
Mason Clayton <masongclayton@gmail.com>
2018-10-16 06:20:13 -0700

    give that boi some criu

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ SRCDIR   := $(TOPDIR)/src
 CC       := gcc
 CFLAGS   := -std=c++11 -O2 -Wall -Wextra -pipe -I./include -fno-rtti -fpic
 V8_LIBS  := -L./lib -lv8_libplatform -lv8_base -lv8_init -lv8_initializers -lv8_libbase -lv8_libsampler -lv8_nosnapshot -lv8_snapshot
-LDFLAGS  := -lstdc++ -lboost_system -pthread -lm $(V8_LIBS)
+LDFLAGS  := -lstdc++ -lboost_system -pthread -lm -lcriu $(V8_LIBS)
 
 SRCS     := $(shell find $(SRCDIR) -type f -name "*.cpp")
 OBJS     := $(patsubst %.cpp,%.o,$(SRCS))
--- src/evaler.cpp
+++ src/evaler.cpp
@@ -1,48 +1,46 @@
-// Copyright 2015 the V8 project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
 #include <libplatform/libplatform.h>
 #include <v8.h>
 
 void run() {
-  // Initialize V8.
-  v8::V8::InitializeICUDefaultLocation("a");
-  v8::V8::InitializeExternalStartupData("a");
   v8::Platform *platform = v8::platform::CreateDefaultPlatform();
   v8::V8::InitializePlatform(platform);
   v8::V8::Initialize();
-  // Create a new Isolate and make it the current one.
+
   v8::Isolate::CreateParams create_params;
   create_params.array_buffer_allocator =
       v8::ArrayBuffer::Allocator::NewDefaultAllocator();
   v8::Isolate *isolate = v8::Isolate::New(create_params);
+
   {
     v8::Isolate::Scope isolate_scope(isolate);
-    // Create a stack-allocated handle scope.
     v8::HandleScope handle_scope(isolate);
-    // Create a new context.
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
-    // Enter the context for compiling and running the hello world script.
+
+    auto context = v8::Context::New(isolate);
     v8::Context::Scope context_scope(context);
-    // Create a string containing the JavaScript source code.
-    v8::Local<v8::String> source =
-        v8::String::NewFromUtf8(
-            isolate, "'Hello' + ', World!'", v8::NewStringType::kNormal)
-            .ToLocalChecked();
-    // Compile the source code.
-    v8::Local<v8::Script> script =
-        v8::Script::Compile(context, source).ToLocalChecked();
-    // Run the script to get the result.
-    v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-    // Convert the result to an UTF8 string and print it.
-    v8::String::Utf8Value utf8(isolate, result);
-    printf("%s\n", *utf8);
+
+    for (;;) {
+      std::string code;
+      std::cin >> code;
+      std::cout << "o" << std::endl;
+
+      auto source = v8::String::NewFromUtf8(
+                        isolate, code.c_str(), v8::NewStringType::kNormal)
+                        .ToLocalChecked();
+
+      auto script = v8::Script::Compile(context, source).ToLocalChecked();
+
+      auto result = script->Run(context).ToLocalChecked();
+
+      v8::String::Utf8Value utf8(isolate, result);
+      printf("%s\n", *utf8);
+    }
   }
-  // Dispose the isolate and tear down V8.
+
   isolate->Dispose();
   v8::V8::Dispose();
   v8::V8::ShutdownPlatform();
